3dbd09c9329385a2445e6a967e1d1fb480e61f6b
Max Belkin <belkin@instories.com>
2022-10-08 12:59:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    for i in range(1, 11):
+        print(fib(i))
+
